257bf69d196306818b9443f792362f9038fd4246
Jessé Haniel do Nascimento Batista <jesse.haniel@phoebus.local>
2019-08-09 18:54:30 -0300

    Configuring initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO BOOK (id, title, author_name, isbn, resume, year_release)
+VALUES (1, 'Mastering Spring 5.0', 'Ranga Karanam', '9781787123175', 'Complete course about Spring 5.0', 2017);
