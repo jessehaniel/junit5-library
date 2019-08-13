29d127750e08d9d0e1a24d6cfba5702d35ab8316
Jessé Haniel do Nascimento Batista <jesse.haniel@phoebus.local>
2019-08-13 11:23:03 -0300

    Beginnig the development for User

--- src/main/java/dev/jessehaniel/phoebus/library/user/User.java
+++ src/main/java/dev/jessehaniel/phoebus/library/user/User.java
@@ -1,12 +1,16 @@
 package dev.jessehaniel.phoebus.library.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 public class User {
@@ -14,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
     private Integer age;
     private String phoneNumber;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,7 @@ VALUES (1, 'Mastering Spring 5.0', 'Ranga Karanam', '9781787123175',
 INSERT INTO BOOK (id, title, author_name, isbn, resume, year_release)
 VALUES (2, 'Mastering Spring 5 - Second Edition', 'Ranga Rao Karanam ', '9781789615692',
         'Build scalable and flexible Rest APIs and microservices using the latest versions of Spring and Spring Boot', 2019);
+
+INSERT INTO USER (id, name, age, phone_number) VALUES (1, 'User 1', 33, '123456');
+INSERT INTO USER (id, name, age, phone_number) VALUES (2, 'User 2', 34, '123456');
+INSERT INTO USER (id, name, age, phone_number) VALUES (3, 'User 3', 35, '123456');
